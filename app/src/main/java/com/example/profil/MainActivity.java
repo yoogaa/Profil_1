477946f1a2c822c7ad77f9d25ccb756bc70e16e3
yoogaa <yudonoprayoga123@gmail.com>
2019-12-28 17:43:30 +0800

    Lokalisasi bahasa

--- app/src/main/java/com/example/profil/MainActivity.java
+++ app/src/main/java/com/example/profil/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.profil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.follow_me);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, getString(R.string.follow_me), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
